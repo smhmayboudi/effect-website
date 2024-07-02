620c8a114d2675f64f2f5c7b58530479757114a4
Hossein Mayboudi <smhmayboudi@gmail.com>
2024-07-02 13:38:06 +0330

    add scheduling

---
+++ src/streaming/stream/scheduling.ts
@@ -0,0 +1,19 @@
+import {Stream, Schedule, Console, Effect} from 'effect';
+
+const stream = Stream.make(1, 2, 3, 4, 5).pipe(
+  Stream.schedule(Schedule.spaced('1 second')),
+  Stream.tap(Console.log)
+);
+Effect.runPromise(Stream.runCollect(stream)).then(console.log);
+/*
+Output:
+1
+2
+3
+4
+5
+{
+  _id: "Chunk",
+  values: [ 1, 2, 3, 4, 5 ]
+}
+*/
